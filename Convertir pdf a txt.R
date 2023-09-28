869536b713f54967d483dcf57755c21dd41e072a
AriadnaAbril <arcontrerasa@unal.edu.co>
2023-09-28 14:46:45 -0500

    TXT 60-90

---
+++ Convertir pdf a txt.R
@@ -0,0 +1,29 @@
+library(pdftools)
+getwd()
+setwd("C:/Users/ASUS/OneDrive/Documentos/Tesis/PDF")
+
+pub <- list.files()
+
+Conv_pdf_txt <- function(pdf){
+  txt <- paste(pdf_text(pdf), collapse="\n")
+  return(txt)
+}
+
+for (i in 1:length(pub)){
+
+  años <- NULL
+  setwd(paste0("C:/Users/ASUS/OneDrive/Documentos/Tesis/PDF/", pub[i]))
+  años <- list.files()
+  for (j in 1:length(años)) {
+    
+    files <- files_txt <- NULL
+    setwd(paste0("C:/Users/ASUS/OneDrive/Documentos/Tesis/PDF/", pub[i],"/", años[j]))
+    files <- list.files(pattern = "\\.pdf$")
+    files_txt <- lapply(files, Conv_pdf_txt)
+    for (k in 1:length(files_txt)) {
+      writeLines(unlist(files_txt[k]), paste0(getwd(),"/",files[k],".txt"))
+    }
+  }
+}
+
+
